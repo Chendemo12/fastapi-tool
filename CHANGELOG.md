0afd270aaccf82332e5bd09c72e23d1b6312d303
lee <ithika@163.com>
2023-06-28 16:23:12 +0800

    feat: add cronjob,logger and utils

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# FastApi-tool
+
+## v0.1.0 - 20230628
+
+### Feat
+
+- 分离`FastApi`的基础工具包
---
+++ version.go
@@ -0,0 +1,7 @@
+package fastapitool
+
+const (
+	Version   = "0.1.0"
+	Copyright = "Chendemo12"
+	Website   = "https://github.com/Chendemo12"
+)
